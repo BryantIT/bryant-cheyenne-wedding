c583457cd95eca0c939159aa06b354f8c62c2eb2
Bryant Richards <dragonseyeimages@gmail.com>
2020-10-09 15:52:58 -0500

    mobile menu component

--- src/components/MobileMenu/index.js
+++ src/components/MobileMenu/index.js
@@ -1,65 +1,76 @@
-import React, { Component } from 'react'
-import AnchorLink from 'react-anchor-link-smooth-scroll'
+import React, { Component } from 'react';
+import AnchorLink from 'react-anchor-link-smooth-scroll';
 import { Collapse, CardBody, Card } from 'reactstrap';
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom';
 import './style.css';
 
 export default class MobileMenu extends Component {
 
-    state = {
-        isMenuShow: false,
-        isOpen: false,
-    }
+  state = {
+    isMenuShow: false,
+    isOpen: false
+  }
 
-    menuHandler = () => {
-        this.setState({
-            isMenuShow: !this.state.isMenuShow
-        })
-    }
+  menuHandler = () => {
+    this.setState({
+      isMenuShow: !this.state.isMenuShow
+    })
+  }
 
-    setIsOpen = () => {
-        this.setState({
-            isOpen: !this.state.isOpen
-        })
-    }
+  setIsOpen = () => {
+    this.setState({
+      isOpen: !this.state.isOpen
+    })
+  }
 
+  render() {
 
-    render() {
+    const {isMenuShow, isOpen} = this.state;
 
-        const { isMenuShow, isOpen } = this.state;
+    return (<div>
+      <div className={`mobileMenu ${isMenuShow
+          ? 'show'
+          : ''}`}>
+        <div className="logo2">
+          <h2>
+            <Link to='/home'>Bryant & Cheyenne</Link>
+          </h2>
+        </div>
+        <ul className="responsivemenu">
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <AnchorLink href='#couple'>Couple</AnchorLink>
+          </li>
+          <li>
+            <AnchorLink href='#story'>Story</AnchorLink>
+          </li>
+          <li>
+            <AnchorLink href='#people'>People</AnchorLink>
+          </li>
+          <li>
+            <AnchorLink href='#event'>Events</AnchorLink>
+          </li>
+          <li>
+            <AnchorLink href='#gallery'>Gallery</AnchorLink>
+          </li>
+          <li>
+            <AnchorLink href='#rsvp'>Rsvp</AnchorLink>
+          </li>
+          <li>
+            <Link to='/blog'>Blog</Link>
+          </li>
+          <li>
+            <Link to='/Blog-details'>Blog Details</Link>
+          </li>
+        </ul>
 
-        return (
-            <div>
-                <div className={`mobileMenu ${isMenuShow ? 'show' : ''}`}>
-                    {/* <div className="clox" onClick={this.menuHandler}>Close Me</div> */}
-                    <div className="logo2">
-                        <h2><Link to='/home'>David & Aliza</Link></h2>
-                    </div>
-                    <ul className="responsivemenu">
-                        <li><p onClick={this.setIsOpen}>Home<i className="fa fa-angle-right" aria-hidden="true"></i></p></li>
-                        <Collapse isOpen={isOpen}>
-                            <Card>
-                                <CardBody>
-                                    <li><Link to='/home'>Home style 1</Link></li>
-                                    <li><Link to='/home2'>Home style 2</Link></li>
-                                    <li><Link to='/home3'>Home video</Link></li>
-                                </CardBody>
-                            </Card>
-                        </Collapse>
-                        <li><AnchorLink href='#couple'>Couple</AnchorLink></li>
-                        <li><AnchorLink href='#story'>Story</AnchorLink></li>
-                        <li><AnchorLink href='#people'>People</AnchorLink></li>
-                        <li><AnchorLink href='#event'>Events</AnchorLink></li>
-                        <li><AnchorLink href='#gallery'>Gallery</AnchorLink></li>
-                        <li><AnchorLink  href='#rsvp'>Rsvp</AnchorLink></li>
-                        <li><Link to='/blog'>Blog</Link></li>
-                        <li><Link to='/Blog-details'>Blog Details</Link></li>
-                    </ul>
-                    
-                </div>
+      </div>
 
-                <div className="showmenu" onClick={this.menuHandler}><i className="fa fa-bars" aria-hidden="true"></i></div>         
-            </div>
-        )
-    }
+      <div className="showmenu" onClick={this.menuHandler}>
+        <i className="fa fa-bars" aria-hidden="true"></i>
+      </div>
+    </div>)
+  }
 }

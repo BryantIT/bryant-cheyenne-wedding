74b30b1cb81a101c08978df103c83cfa5c311d1d
Bryant Richards <dragonseyeimages@gmail.com>
2020-10-11 22:58:51 -0500

    conditional rendering works

--- src/components/rsvp/index.js
+++ src/components/rsvp/index.js
@@ -4,7 +4,13 @@ import Confirmation from '../Confirmation'
 import firebase from '../../firebase'
 import './style.css'
 
+const secretCode = process.env.REACT_APP_INVITE_CODE
+
 export default function Rsvp () {
+  const [invited, setInvited] = useState('')
+  const [shouldRender, setShouldRender] = useState(false)
+  const [secretsRender, setSecretsRender] = useState('')
+  const [inviteCode, setInviteCode] = useState('')
   const [formData, setFormData] = useState({
     name: '',
     email: '',
@@ -38,10 +44,35 @@ export default function Rsvp () {
   const handleChange = (e) => {
     setFormData({...formData, [e.target.name]: e.target.value})
   }
-  console.log('HANDLE CHANGE', formData)
+
+  useEffect(() => {
+    const temp = invited
+    if (!temp) {
+      setShouldRender(false)
+    } else {
+      setShouldRender(true)
+    }
+  }, [invited])
+
+  const handleInviteChange = (e) => {
+    setInvited(e.target.value)
+  }
+
+  useEffect(() => {
+    if (inviteCode !== secretCode) {
+      setSecretsRender(false)
+    } else {
+      setSecretsRender(true)
+    }
+  }, [inviteCode])
+
+  const handleSecretChange = (e) => {
+    setInviteCode(e.target.value)
+  }
 
   const handleSubmit = (e) => {
     e.preventDefault()
+    const guestId = idGenerator
     const name = formData.name
     const email = formData.email
     const rsvp = formData.rsvp
@@ -51,6 +82,7 @@ export default function Rsvp () {
     .firestore()
     .collection('guests')
     .add({
+      guestId,
       name,
       email,
       rsvp,
@@ -95,28 +127,51 @@ export default function Rsvp () {
             <div className="rsvp-wrap">
               <form onSubmit={handleSubmit}>
                 <div className="contact-form form-style">
+                  <div className="col col-sm-12">
+                    <select className="form-control" required onChange={handleInviteChange} value={invited} name="rsvp">
+                      <option disabled="disabled" value="">Do you have an invite code?*</option>
+                    <option value="Yes">Yes</option>
+                  <option value="No">No</option>
+                    </select>
+                  </div>
+                  {
+                    shouldRender ?
+                    <div className="col-12 col-sm-12">
+                      <input type="text" value={inviteCode} onChange={handleSecretChange} placeholder="Your Code*" id="fname" name="inviteCode"/>
+                  </div> : null
+                  }
                   <div className="col-12 col-sm-12">
-                    <input type="text" value={formData.name} onChange={handleChange} placeholder="Your Name*" id="fname" name="name"/>
+                    <input type="text" value={formData.name} required onChange={handleChange} placeholder="Your Name*" id="fname" name="name"/>
                   </div>
                   <div className="col-12  col-sm-12">
-                    <input type="text" placeholder="Your Email*" onChange={handleChange} value={formData.email} id="email" name="email"/>
+                    <input type="text" placeholder="Your Email*" onChange={handleChange} value={formData.email} required id="email" name="email"/>
                   </div>
                   <div className="col col-sm-12">
-                    <select className="form-control" onChange={handleChange} value={formData.rsvp} name="rsvp">
+                    <select className="form-control" onChange={handleChange} value={formData.rsvp} required name="rsvp">
                       <option disabled="disabled" value="">Number Of rsvp*</option>
+                    <option value="0">0</option>
                       <option value="1">1</option>
                       <option value="2">2</option>
                       <option value="3">3</option>
                       <option value="4">4</option>
                     </select>
                   </div>
-                  <div className="col col-sm-12">
-                    <select className="form-control" onChange={handleChange} value={formData.events} name="events">
-                      <option disabled="disabled" value="">I Am Attending*</option>
-                    <option value="In Spirit">In Spirit</option>
-                  <option value="In Person">In Person</option>
-                    </select>
-                  </div>
+                  {
+                    secretsRender ?
+                    <div className="col col-sm-12">
+                      <select className="form-control" onChange={handleChange} value={formData.events} required name="events">
+                        <option disabled="disabled" value="">I Am Attending*</option>
+                      <option value="In Person">In Person</option>
+                      <option value="In Spirit">In Spirit</option>
+                      </select>
+                    </div> :
+                    <div className="col col-sm-12">
+                      <select className="form-control" onChange={handleChange} value={formData.events} required name="events">
+                        <option disabled="disabled" value="">I Am Attending*</option>
+                      <option value="In Spirit">In Spirit</option>
+                      </select>
+                    </div>
+                  }
                   <div className="col-12 col-sm-12">
                     <textarea className="contact-textarea" value={formData.notes} onChange={handleChange} placeholder="Message" name="notes"></textarea>
                   </div>

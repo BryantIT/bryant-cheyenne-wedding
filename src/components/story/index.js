d557159a8a26746d7f7d8da34677629bbc6b3261
Bryant Richards <dragonseyeimages@gmail.com>
2020-10-30 19:43:32 -0500

    fix type

--- src/components/story/index.js
+++ src/components/story/index.js
@@ -1,28 +1,28 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import Sectiontitle from "../section-title";
-import strory1 from "../../images/story/img-1.jpg";
-import strory2 from "../../images/story/img-2.jpg";
-import strory3 from "../../images/story/img-3.jpg";
-import strory4 from "../../images/story/img-4.jpg";
-import "./style.css";
+import React from 'react'
+import { Link } from 'react-router-dom'
+import Sectiontitle from '../section-title'
+import story1 from '../../images/story/img-1.jpg'
+import story2 from '../../images/story/img-2.jpg'
+import story3 from '../../images/story/img-3.jpg'
+import story4 from '../../images/story/img-4.jpg'
+import './style.css'
 
 const Story = () => {
   return (
-    <div id="story" className="story-area section-padding">
-      <Sectiontitle section={"Our Love Story"} />
-      <div className="container">
-        <div className="story-wrap">
-          <div className="row">
-            <div className="col-lg-6 col-md-12 col-12 pr-n">
-              <div className="story-img">
-                <img src={strory1} alt="" />
+    <div id='story' className='story-area section-padding'>
+      <Sectiontitle section={'Our Love Story'} />
+      <div className='container'>
+        <div className='story-wrap'>
+          <div className='row'>
+            <div className='col-lg-6 col-md-12 col-12 pr-n'>
+              <div className='story-img'>
+                <img src={story1} alt="" />
               </div>
             </div>
-            <div className="col-lg-6 col-md-12 col-12">
-              <div className="story-text left-align-text">
+            <div className='col-lg-6 col-md-12 col-12'>
+              <div className='story-text left-align-text'>
                 <h3>First time we met</h3>
-                <span className="date">Jan 12 2019</span>
+                <span className='date'>Jan 12 2019</span>
                 <p>
                   A wonderful serenity has taken possession of my entire soul,
                   like these sweet mornings of spring which I enjoy with my
@@ -30,8 +30,8 @@ const Story = () => {
                   this spot, which was created for the bliss of souls like mine.
                   I am so happy, my dear friend,{" "}
                 </p>
-                <div className="story-button">
-                  <Link className="theme-btn" to="/">
+                <div className='story-button'>
+                  <Link className='theme-btn' to='/'>
                     Read More
                   </Link>
                 </div>
@@ -57,12 +57,12 @@ const Story = () => {
             </div>
             <div className="col-lg-6 col-md-12 col-12 pl-n">
               <div className="story-img">
-                <img src={strory2} alt="" />
+                <img src={story2} alt="" />
               </div>
             </div>
             <div className="col-lg-6 col-md-12 col-12 pr-n">
               <div className="story-img">
-                <img src={strory3} alt="" />
+                <img src={story3} alt="" />
               </div>
             </div>
             <div className="col-lg-6 col-md-12 col-12">
@@ -103,7 +103,7 @@ const Story = () => {
             </div>
             <div className="col-lg-6 col-md-12 col-12 pl-n">
               <div className="story-img">
-                <img src={strory4} alt="" />
+                <img src={story4} alt="" />
               </div>
             </div>
           </div>
